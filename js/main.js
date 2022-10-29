0c70fa3932eacc38f7c6812804b6ce1e87a54223
rakhmonov <bohodir29102001@gmail.com>
2022-10-29 21:00:46 +0500

    First commit

---
+++ js/main.js
@@ -0,0 +1,63 @@
+// const first_name = "Bohodir";
+
+// console.log(first_name);
+
+// function nimadir() {
+//   var firstName = "Bohodir";
+// }
+
+// console.log(firstName);
+// firstName = "Xurshid";
+// let activeName = "Xurshid";
+
+// const name = "Bohodir";
+// const num = 12.8;
+// const boolean = true;
+// const yoqQiymat = null;
+// let oZgaruvchi;
+// // let kattaSon =
+// let id = Symbol("id");
+
+// let obj = {}
+
+// console.log(name);
+
+// let name = "Bohodir";
+
+// let name2 = name; "Bohodir"
+
+// name2 = "Xurshid";
+
+// console.log(name2);
+
+// //// No primative
+// let obj = {
+//   name: "Bohodir",
+// };
+
+// let obj2 = obj;
+
+// obj2.lastName = "Rahmonov";
+
+// console.log("obj", obj, "obj2", obj2);
+
+// const name = prompt("Ismingiz nima");
+
+// alert(name);
+
+// let sum = "10";
+
+// if (sum == 10) {
+//   console.log("Javob teng");
+// } else {
+//   console.log("Javob  teng emas");
+// }
+
+const lastName = "Rahmonov";
+// const name = "Ozbekiston";
+
+// const name = `Bo'hodir
+//  ${lastName}
+//  "22 yosh"`;
+
+// console.log(name);
