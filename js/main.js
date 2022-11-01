35e6242ec377254b89e7ab11a998222fe4b0ef7f
rakhmonov <bohodir29102001@gmail.com>
2022-11-01 21:19:54 +0500

    Second commit

---
+++ js/main.js
@@ -0,0 +1,101 @@
+const expense = 500;
+const oneDollar = 1180.5;
+// let userMoney = prompt("O'zingizda qancha pul bor");
+// userMoney = parseInt(userMoney, 10);
+
+// if (userMoney) {
+//   let convertedDollar = userMoney / oneDollar;
+//   console.log(convertedDollar);
+
+//   if (expense < convertedDollar) {
+//     console.log("Siz sayohatga bemalol bora olasiz");
+//   } else if (expense === convertedDollar) {
+//     console.log("Har ehtimolga qarshi qoshimcha maglag olib oling");
+//   } else if (expense === 0) {
+//     console.log("dsds");
+//   } else {
+//     console.log("Siz sayohatga bora olamaysiz");
+//   }
+
+//   switch (convertedDollar) {
+//     case expense < convertedDollar:
+//       console.log("Siz sayohatga bemalol bora olasiz");
+//       break;
+
+//     case convertedDollar === expense:
+//       console.log("Har ehtimolga qarshi qoshimcha maglag olib oling");
+//       break;
+
+//     default:
+//       console.log("Siz sayohatga bora olamaysiz");
+//   }
+// }
+
+// const status = 3;
+
+// switch (status) {
+//   case 0:
+//     console.log("Siz sayohatga bemalol bora olasiz");
+//     break;
+//   case 1:
+//     console.log("Har ehtimolga qarshi qoshimcha maglag olib oling");
+//     break;
+//   default:
+//     console.log("Siz sayohatga bora olamaysiz");
+//     break;
+// }
+
+// const num = 10;
+// if(num >= 10){
+
+// }
+
+// let str = "                    Bohodir                        ";
+
+// str = str.concat(" Rahmonov");
+
+// console.log(str.trim());
+
+// console.log(str.length);
+
+// let userName = prompt("Ismingiz");
+
+// console.log(userName.trimStart());
+
+// console.log(userName.indexOf("B"));
+// console.log(userName.indexOf("B"));
+
+// const str = "Bohodir Rahmonov";
+
+// str.substring(1, 3);
+
+// console.log(str);
+
+// let bookShelf = ["Book1", "Book2", "Book3"];
+
+// console.log(bookShelf.join(" "));
+
+// let books = "Book1-Book2-Boook3";
+
+// console.log(books.split("-"));
+
+// const userName = document.getElementById("name");
+// const username2 = document.getElementsByClassName("name");
+// const username3 = document.getElementsByTagName("h1");
+
+// const userName4 = document.querySelectorAll("h1.name");
+
+// console.log(userName4);
+
+// console.log();
+
+// username2[0].style.fontSize = "70px";
+// userName.addEventListener("click", () => {
+//   //   userName.classList.toggle("h1");
+
+//   if (userName.classList.contains("h1")) {
+//     userName.classList.remove("h1");
+//   } else {
+//     userName.classList.add("h1");
+//   }
+// });
