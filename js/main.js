12d27c6b4a775d44d299b4c1bd5e69be9269d2b6
rakhmonov <bohodir29102001@gmail.com>
2022-11-03 22:01:17 +0500

    fix

---
+++ js/main.js
@@ -0,0 +1,89 @@
+// console.log(typeof 0);
+
+// console.log(Math.random());
+
+// function random(){
+
+// }
+
+// console.log(Math.min(1, 2, 3, 10, 7, 30));
+
+//Math.round() =>  Bizda sonlarni aniq yaxlitlash uchun ishlatamiz.
+// Math.ciel = > Bizda sonlarni iloji boricha katta midoqrda yaxlitlaydi.
+// Math.floor => Bizda sonlarni iloji boricha kichkina miqdorda yaxlitlaydi.
+// Math max => eng katta sonni olib berish uchun xizmat qiladi min esa teskarisi Math.min
+// let numberSByString = "30";
+
+// numberSByString = parseInt(numberSByString, 10);
+
+// console.log(numberSByString);
+
+// const num = 10;
+
+// console.log(num.toString());
+
+// console.log(Math.random().toFixed(2));
+
+// function tofixed(num,number) {
+// Number.toString().slice;
+
+//   String.slice(0, num);
+// }
+
+// console.log(+'10')
+
+// And OR Not
+
+// let agree = prompt("Qarshimisiz yoki qarshimsiz (Ehtimoliy javoblar y yoki n)");
+
+// if (agree === "y" || agree === "Y") {
+//   console.log("Agreed");
+// } else if (agree === "n" || agree === "N") {
+//   console.log("No agreed");
+// }
+
+// 14 yoshdan 31 gacha holatda siz yoshsiz
+
+// 14 yoshdan kichkina ota yoshsiz
+// 31 yoshdan  orta yoshsiz
+
+// let age = +prompt("Yoshiz nechida");
+// if (age >= 14 && age <= 31) {
+//   alert("siz yoshsiz ");
+// } else if (age < 14) {
+//   alert("Siz o'ta yoshsiz");
+// } else if (age > 31) {
+//   alert("Siz orta yoshsiz");
+// }
+// const bool = 10;
+// if (bool === 10) {
+//   console.log("Something");
+// } else {
+//   // cons;
+// }
+
+// if (false) {
+//   console.log("bu bir raqami");
+// } else {
+//   console.log("Boshqa raqam");
+// }
+
+// alert(alert(1) || 2 || alert(3));
+
+// alert(null && 1);
+
+// console.log(0 && 1);
+
+// const str = "";
+
+// console.log(!str.length && "Yozuv yoq");
+
+// let age = +prompt("Yoshingiz");
+
+// if (age >= 14 && age <= 90) {
+//   console.log("Siz tiriksiz");
+// }
+
+// if (age < 14 || age > 90) {
+//   console.log("Siz tiriksiz");
+// }
