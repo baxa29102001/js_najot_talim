89c4d00be8c81b7c1993934a7729fcf0ae03eb05
rakhmonov <bohodir29102001@gmail.com>
2022-11-05 21:51:44 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,94 @@
+// console.log("Bir qadam yurdim");
+// console.log("Bir qadam yurdim 2");
+// console.log("Bir qadam yurdim 3");
+// console.log("Bir qadam yurdim 4");
+// console.log("Bir qadam yurdim 5");
+// console.log("Bir qadam yurdim 6");
+// console.log("Bir qadam yurdim 7");
+
+// let name ="Xurshid"
+
+// function stepByStep(step, text = "Bir qadam yurdim") {
+//   let name = "Bohodir";
+
+//   console.log(name);
+// }
+
+// stepByStep(1, "onga yurdim");
+// stepByStep(2);
+// stepByStep();
+// stepByStep(4);
+// stepByStep(5);
+
+// let name = "Xurshid";
+
+// function checkGlobalVariable() {
+//     name = "Bohodir";
+//     console.log(name);
+// }
+
+// console.log(name);
+
+// checkGlobalVariable();
+
+// function sumUpHandler(a, b, action) {
+//   if (action === "plus") {
+//     let sum = a + b;
+//     console.log(sum);
+//     // return sum;
+//   }
+//   console.log("Ishla");
+//   let minus = b - a;
+
+//   return minus;
+// }
+
+// console.log(sumUpHandler(3, 4, "plus"));
+
+// function sleepDay(weekday, vacation) {
+//   if (!weekday || vacation) return true;
+//   //   else if()
+
+//   return false;
+// }
+
+// console.log(sleepDay(true, true)); // → true
+// console.log(sleepDay(true, false)); // → false
+// console.log(sleepDay(false, true)); // → true
+
+// function monkeyTrouble(aSmile, bSmile) {
+//   if ((aSmile && bSmile) || !(aSmile && bSmile)) return true;
+
+//   return false;
+// }
+
+// monkeyTrouble(true, true) → true
+// monkeyTrouble(false, false) → true
+// monkeyTrouble(true, false) → false
+
+// // let time = 8
+// // if(time< 7 || time > 20)
+
+// function toti(talking,time){
+//     if(talking && time< 7 || time > 20)
+// }
+
+//  ================== Function Declaration ===================
+// getLastValue();
+// function getLastValue() {
+//   console.log("Consoole holati");
+// }
+// name = 'dsds'
+
+// let name ="Bohoir"
+
+/// ========= Function expression ===================
+// const getLastValue = function(){
+//     console.log("Hello world")
+// }
+
+// ====== Arrow  function ========
+// const getLastValue = () => {
+//   console.log("Hello World");
+// };
+// getLastValue();
