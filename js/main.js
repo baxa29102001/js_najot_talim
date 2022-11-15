503f2c1efeadff91e5c69f2d6a4730a817016f53
rakhmonov <bohodir29102001@gmail.com>
2022-11-15 20:45:32 +0500

    array methods

---
+++ js/main.js
@@ -0,0 +1,79 @@
+// let user = {
+//   name: "Bohodir",
+//   age: 22,
+// };
+
+// let obj2 = { ...user };
+// let obj3 = {
+//   lastName: "Rahmonov",
+// };
+// // let admin = Object.assign(user, obj3);
+// let obj2 = {
+//   ...user,
+//   // lastName: "Rakhonov",
+//   obj3,
+// };
+// console.log(obj2);
+
+// console.log(user === obj2);
+
+// const
+
+// const arr = []
+// arr.splice()
+
+// console.log(arr.)
+
+// arr.forEach((element, index, array) => {
+//   console.log(array);
+
+//   // return
+// });
+
+// function forEach() {
+//   // for (let i = 0; i < arr.length; i++) {
+//   //   console.log(arr[i]);
+//   // }
+// }
+
+// let arr2 = arr.map((element, index, arr) => {
+//   return element * 2;
+// });
+
+// let obej = {}
+// let obej2 = {}
+
+// let arr2 = arr.map((element, index, arr) => {
+//   return element;
+// });
+
+// const arr = [5, 1, 4, 6, 8];
+
+// let arr2 = arr.slice(2);
+
+// console.log(arr);
+
+// console.log(arr2);
+// console.log(arr2);
+
+// function mapFun(arr) {
+//   let result = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     result.push(arr[i] * 2);
+//   }
+
+//   return result;
+// }
+
+// console.log(arr2);
+
+// let arr2 = arr.filter((element, index, arr) => {
+//   return element === 5;
+// });
+
+// console.log(arr2);
+
+// let arr2 = arr.sort((a, b) => a - b);
+// let arr2 = arr.reverse();
+
+// console.log(arr2);
