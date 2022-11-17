6c840b969ba256028708baacf3288622b46207bb
rakhmonov <bohodir29102001@gmail.com>
2022-11-17 20:16:55 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,68 @@
+// for (let i = 0; i < 6; i++) {
+//   // if (i === 2) continue;
+//   // if (i === 2) break;
+//   console.log("Birinchi loop", i);
+//   for (let j = 1; j < 5; j++) {
+//     console.log("Ikkinchi loop");
+
+//     if (j === 2) break;
+//   }
+// }
+
+const arr = [
+  {
+    arr: ["Salom", "Nima gap"],
+
+    id: 4,
+  },
+  {
+    arr: ["Ikkinchi malumot"],
+    id: 6,
+  },
+];
+
+// name: for (let i = 0; i < arr.length; i++) {
+//   for (let j = 0; j < arr[i].arr.length; j++) {
+//     if (arr[i].arr[j] === "Nima gap") continue name;
+//     console.log(arr[i].arr[j]);
+//   }
+// }
+
+// const arr = ["Hello", "World", "Salom", "Dunyo"];
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// for (let item of arr) {
+//   for (let j of item.arr) {
+//     console.log(j);
+//   }
+// }
+// let obj = {
+//   name: "Bohodir",
+//   lastName: "Rahmonov",
+// };
+
+// // for (let key in obj) {
+// //   console.log(obj[key]);
+// // }
+
+// ["name", "lastName"];
+
+// Object.keys(obj).forEach((item) => {
+//   console.log(item);
+// });
+
+// Object.values(obj).forEach((item) => {
+//   console.log(item);
+// });
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i].id === 4) console.log(arr[i]);
+// }
+
+// let obj = arr.find((_, index, arr) => item.id === 4);
+// let obj = arr.findIndex((item, index, arr) => item.id === 4);
+
+// console.log(obj);
