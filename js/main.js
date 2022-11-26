d2d05986862f357b5639e154d911c6cb43b6ea5a
rakhmonov <bohodir29102001@gmail.com>
2022-11-26 19:50:43 +0500

    first commit

--- js/main.js
+++ js/main.js
@@ -4,23 +4,6 @@ const sausage = document.querySelector("#sausage");
 
 const checkbox = document.querySelectorAll(".checkbox");
 let onPizzaArray = [];
-function getCheckboxValue(e) {
-  console.log(e.target.id);
-  // console.log(e.target.checked);
-
-  if (e.target.checked) onPizzaArray.push(e.target.id);
-  else onPizzaArray = onPizzaArray.filter((item) => item !== e.target.id);
-
-  // onPizzaArray.push(e.target.id);
-
-  console.log(onPizzaArray);
-}
-
-// tomato.addEventListener("change", getCheckboxValue);4
-
-checkbox.forEach((item) => {
-  item.addEventListener("change", getCheckboxValue);
-});
 
 const pizzaOrder = {
   userName: "Palonchi",
@@ -29,6 +12,26 @@ const pizzaOrder = {
 
   thickness: 2,
 
-  onPizza: ["Tomato", "dsdsds"],
-  addPizza: ["dsdsd", "dsdsds"],
+  onPizza: [],
+  addPizza: [],
 };
+function getCheckboxValue(e) {
+  console.log(e.target.id);
+  // console.log(e.target.checked);
+
+  if (e.target.checked) pizzaOrder.onPizza.push(e.target.id);
+  else
+    pizzaOrder.onPizza = pizzaOrder.onPizza.filter(
+      (item) => item !== e.target.id
+    );
+
+  // onPizzaArray.push(e.target.id);
+
+  console.log(pizzaOrder.onPizza);
+}
+
+// tomato.addEventListener("change", getCheckboxValue);4
+
+checkbox.forEach((item) => {
+  item.addEventListener("change", getCheckboxValue);
+});
