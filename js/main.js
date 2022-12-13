0a248c2a94e71c810cbc0a7121e0586ae6492ed0
rakhmonov <bohodir29102001@gmail.com>
2022-12-13 16:15:10 +0500

    fix

--- js/main.js
+++ js/main.js
@@ -1,40 +1,126 @@
-const name = "Bohodir";
+// const ibrohim = {
+//   name: "Ibrohim",
+//   lastName: "Hakimov",
+//   age: 22,
+//   fullName() {
+//     return this.name + " " + this.lastName;
+//   },
+// };
 
-function helloWorld() {
-  console.log("Hello World");
+// const sodiqjon = {
+//   ame: "Ibrohim",n
+//   lastName: "Hakimov",
+//   ageNum: 22,
+//   fullName() {
+//     return this.name + " " + this.lastName;
+//   },
+// };
+// console.log(sodiqjon.age);
+// const arr = [];
+
+// arr.push(ibrohim);
+
+// function fullName() {
+//   return this.lastName + " " + this.name;
+// }
+
+// function Person(name, lastName, age) {
+//   this.name = name;
+//   this.lastName = lastName;
+//   this.ageNum = age;
+// }
+
+// Person.prototype.fullName = fullName;
+
+// const ibrohim = new Person("Ibrohim", "Hakimov", 22);
+// const sodiqjon2 = new Person("Sodiqjon", "Nurmirzayev", 19);
+
+// console.log(sodiqjon2.fullName());
+// console.log(ibrohim.fullName());
+
+// class Person {
+//   constructor(name, lastName, age) {
+//     this.name = name;
+//     this.lastName = lastName;
+//     this.ageNum = age;
+//   }
+
+//   fullName() {
+//     return this.lastName + " " + this.name;
+//   }
+// }
+
+// const ibrohim = new Person("Ibrohim", "Hakimov", 22);
+
+const arr = [
+  "Bohodir",
+  "Sodiqjon",
+  "Ibrohim",
+  "Javlon",
+  "Axborxoja",
+  "Qobil",
+  "Islom",
+  "Islom",
+  "Islom",
+  "Islom",
+  "Islom",
+  "Islom",
+  "Islom",
+  "Islom",
+  "Islom",
+  "Islom",
+  "Islom",
+  "Islom",
+  "Islom",
+  "Islom",
+  "Islom",
+  "Islom",
+  "Islom",
+  "Islom",
+  "Islom",
+  "Islom",
+  "Islom",
+  "Islom",
+  "Islom",
+  "Islom",
+];
+
+const pageUl = document.querySelector(".numbers");
+const names = document.querySelector(".names");
+const limit = 2;
+
+function pagination() {
+  const countPage = Math.ceil(arr.length / limit);
+
+  renderPages(countPage);
 }
 
-// setTimeout(() => {
-//   console.log("Set TimeOut 1");
-// }, 2000);
+function renderPages(num) {
+  let result = "";
+  if (num > 10) {
+    for (let i = 0; i < 5; i++) {
+      if (i === 4) result += ` <li onclick="nameSlice(${i + 1})">...</li>`;
+      result += ` <li onclick="nameSlice(${i + 1})">${i + 1}</li>`;
+    }
+  } else {
+    for (let i = 0; i < num; i++) {
+      result += ` <li onclick="nameSlice(${i + 1})">${i + 1}</li>`;
+    }
+  }
 
-// setInterval(() => {
-//   console.log("Set interval");
-// }, 1000);
+  pageUl.innerHTML = result;
+}
 
-// console.log("Simple Hello World");
+function nameSlice(page) {
+  let startToSlice = page * limit - limit;
+  const arr2 = arr.slice(startToSlice, limit * page);
 
-// helloWorld();
-
-let promise = new Promise((resolve, reject) => {
-  let result = setTimeout(() => {
-    resolve("Muaviqayatli amalga oshdi");
-  }, 3000);
-
-  return result;
-});
-
-console.log("Loading", true);
-
-promise
-  .then((res) => {
-    console.log(res);
-  })
-  .catch((error) => {
-    console.log(error);
-  })
-  .finally(() => {
-    console.log("Finally");
-
-    console.log("Loading", false);
+  let result = "";
+  arr2.forEach((item) => {
+    result += `<li>${item}</li>`;
   });
+
+  names.innerHTML = result;
+}
+
+pagination();
